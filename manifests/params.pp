00cf12a274a68e3c3bee662ec7f8a017c01af869
Nikita <luckyraul@users.noreply.github.com>
2020-12-10 22:42:46 +0300

    bump 0.12.9

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@
 # It sets variables according to platform
 #
 class nomad::params {
-  $version  = '0.12.0'
+  $version  = '0.12.9'
   $url_base = 'https://releases.hashicorp.com/nomad/'
 
   $service_url  = 'https://raw.githubusercontent.com/hashicorp/nomad/master/dist/systemd/nomad.service'
